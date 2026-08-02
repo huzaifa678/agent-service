1ae9c15df349d3be9e89c277360ff5b051a7cae5
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-08-03 01:16:25 +0500

    test(bootstrap): add bootstrap tests

---
+++ agent-service/agent-bootstrap/src/test/java/com/project/agent/domain/bootstrap/AgentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.agent.domain.bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
